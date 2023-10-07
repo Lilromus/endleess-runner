1e47c4b537ff90c899b625701eff1c9883c50e1b
Lilromus <romariokruk@gmail.com>
2023-10-07 19:39:13 +0200

    Update Player.cs

--- Endless_Runner/Assets/Player.cs
+++ Endless_Runner/Assets/Player.cs
@@ -15,6 +15,10 @@ public class Player : MonoBehaviour
     //public bool isHoldingJump = false; ? nie dokonca niewiem czy warto na innych lokach 
 
 // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
