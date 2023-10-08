1b438294c0b08c8866cb9ac3ae05e7ac384336e8
Lilromus <romariokruk@gmail.com>
2023-10-08 21:19:35 +0200

    JAPIERDOLEALE SYF
    dodane tło(niech ktos ich zrobi sa przykladowe w Assets>Scenes. A i jak jestes bardzo chetny w robieniu tego syfu, to mozesz naprawic blad z tym Scorerm(w pliku UIController.cs jest kod tego guwna) Score niechce sie krementowac bo cos tam cos tam. Ten commit jest do naprawy

--- Endless_Runner/Assets/Player.cs
+++ Endless_Runner/Assets/Player.cs
@@ -8,7 +8,11 @@ public class Player : MonoBehaviour
     
     public float gravity;
     public Vector2 velocity;
-    public float groundHeight = -3;
+    public float maxAcceleration = 10;
+    public float acceleration = 10;
+    public float maxXVelocity = 100;
+    public float distance = 0; 
+    public double groundHeight = -3.5;
     public float jumpVelocity = 20;
     public bool isGrounded = false;
 
@@ -21,14 +25,15 @@ public class Player : MonoBehaviour
 // Start is called before the first frame update
     void Start()
     {
-        
+        //yy
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 pos = transform.position;
-        float groundDistance = Mathf.Abs(pos.y - groundHeight);
+        float groundDistance = Mathf.Abs((float)(pos.y - groundHeight));
         if(isGrounded || groundDistance <= jumpGroundThreshold)
         {
             if(Input.GetKeyDown(KeyCode.Space))
@@ -58,7 +63,7 @@ public class Player : MonoBehaviour
                 holdJumpTime += Time.fixedDeltaTime;
                 if(holdJumpTimer >= maxHoldJumpTime)
                 {
-                    isHoldingJump = false;
+                    isHoldingJump = false;]=0
                 }
             }*/
             pos.y += velocity.y * Time.fixedDeltaTime;
@@ -68,9 +73,25 @@ public class Player : MonoBehaviour
             //}
             if(pos.y <= groundHeight)
             {
-                pos.y = groundHeight;
+                pos.y = (float)groundHeight;
                 isGrounded = true;
                 //holdJumpTimer = 0;
+            }
+        }
+
+        distance += velocity.x * Time.fixedDeltaTime;
+
+
+
+
+        if(isGrounded)
+        {
+            float velocityRatio = velocity.x / maxXVelocity;
+            acceleration = maxAcceleration * (1 - velocityRatio);
+            velocity.x += acceleration * Time.fixedDeltaTime;
+            if(velocity.x >= maxXVelocity)
+            {
+                velocity.x = maxXVelocity;
             }
         }
 
